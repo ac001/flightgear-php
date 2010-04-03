68ca98c6c29f3e6abbff002411b9298fe36dd228
Peter Mash Morgan <ac001@daffodil.uk.com>
2010-04-03 11:30:54 +0100

    CVS

--- CVS_LOCAL/checkout_fg.sh
+++ CVS_LOCAL/checkout_fg.sh
@@ -2,12 +2,12 @@
 
 
 cvs -d :pserver:cvsguest@cvs.flightgear.org:/var/cvs/FlightGear-0.9 login
-CVS passwd: guest
+#CVS passwd: guest
 
 cvs -d :pserver:cvsguest@cvs.flightgear.org:/var/cvs/FlightGear-0.9 co fg_data
 
 
 cvs -d :pserver:cvsguest@cvs.flightgear.org:/var/cvs/FlightGear-0.9 login
-CVS passwd: guest
+#CVS passwd: guest
 
 cvs -d :pserver:cvsguest@cvs.flightgear.org:/var/cvs/FlightGear-0.9 co fg_source
