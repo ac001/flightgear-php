413bc3609a7eb79c8581013f8e66b422d6218e14
Peter Mash Morgan <ac001@daffodil.uk.com>
2010-04-05 14:11:05 +0100

    tidy

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ $Site = new fgSite('portal', 'FlightGear Portal');
 
 $Site->addPageNav('index', 'Welcome');
 $Site->addPageNav('servers', 'Servers');
-$Site->addPageNav('signup', 'Sign Up');
+//$Site->addPageNav('signup', 'Sign Up');
 
 $smarty->assign('nav_include', 'portal/nav_inc.html');
 
