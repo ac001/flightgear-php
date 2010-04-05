ea313a2e7bb3889a61f21e4739f22bdcaa2f0ee4
Peter Mash Morgan <ac001@daffodil.uk.com>
2010-04-05 21:24:36 +0100

    layout

--- js/fgAircraftBrowser.js
+++ js/fgAircraftBrowser.js
@@ -6,42 +6,24 @@ var self = this;
 
 
 
-/*
-this.frmAero = new Ext.form.FormPanel({
-	title: '<b>Authors</b>',
-	items: [
-		{fieldLabel: 'Aero', xtype: 'textfield',  name: 'aero'},
-		{fieldLabel: 'Flight Model', xtype: 'textfield',  name: 'fdm', width: '20%'},
-		{fieldLabel: 'Firectory', xtype: 'textfield',  name: 'directory'}
-	]
-
-});
-*/
-
-
-
-
 /****************************************************/
 /** Aircraft Serrch Widget **/
 /****************************************************/
 this.aircraftGrid = new fgAircraftGrid();
 this.aircraftGrid.grid.on('aero_selected', function(aero_id){
-	//Ext.fg.msg(aero_id);
-	//return
 	//self.statusBar.getEl().mask('Loading ');
 	Ext.Ajax.request({
 		url: AJAX_FETCH,
 		params: { fetch: 'aero_info', aero_id: aero_id },
 		success: function(response, opts) {
+
 			var json = Ext.decode(response.responseText);
-			//console.log(json);
-			//self.keysStore.loadData(json.info);
-			//var img = document.getElementById('aero_image')
-			//console.log(img)
-			self.aeroPanel.setTitle(json.aero.name)
+
+			self.aeroMainWidget.setTitle(json.aero.name)
 			//img.setAttribute('src', json.info.thumbnail);
-			console.log(json.images);
-			console.log(json.images.splash);
+
+			//console.log(json.images);
+			//console.log(json.images.splash);
 			var img = document.getElementById('aero_splash')
 			img.setAttribute('src', json.images.splash);
 
@@ -59,38 +41,12 @@ this.aircraftGrid.grid.on('aero_selected', function(aero_id){
 	});
 });
 
-
-
-// second tabs built from JS
-this.tabPanel = new Ext.TabPanel({
-        //layout: 'accordion',
-		border: false,
-        activeTab: 0,
-        plain:false,
-		height: 600,
-        defaults:{autoScroll: true},
-        items:[
-				//this.grid,
-				//this.frmAero,
-				{ title: '<b>Details</b>', html: '', id: 'aero_details_tab'},
-				{ title: '<b>Splash</b>', html: '<img src="images/no_image.gif" id="aero_splash">'},
-				{ title: '<b>Help</b>', html: '', id: 'aero_help_tab'},
-        ]
-	
-});   
-
-this.aeroPanel = new Ext.Panel({
-	//layout: 'border',
-	border: false,
-	title: '--',
-	renderTo: 'aero_main_widget',
-	plain: false,
-	items: [ this.tabPanel ]
-});
+//***************************************************
 
 /****************************************************/
 /** Keyboard **/
 /****************************************************/
+/*
 this.keysStore = new Ext.data.JsonStore({
 	root: 'keyboard',
 	fields: [ 'key', 'description' ]
@@ -98,7 +54,7 @@ this.keysStore = new Ext.data.JsonStore({
 this.keysGrid = new Ext.grid.GridPanel({
 	title: 'Keyboard',
 	renderTo: 'aero_keyboard_widget',
-	height: 200,
+	//height: 200,
 	plain: false,
 	autoScroll: true,
 	enableHdMenu: false,
@@ -108,6 +64,59 @@ this.keysGrid = new Ext.grid.GridPanel({
 				{header: 'Description',  dataIndex:'description', sortable: true},
 	]
 });
+*/
+
+/*
+this.aeroRightCol = new Ext.Panel({
+	layout: 'border',
+	border: false,
+	title: '--',
+	renderTo: 'aero_main_widget',
+	plain: false,
+	items: [ this.keysGrid ]
+});
+*/
+
+this.aeroPanel = new Ext.Panel({
+        //layout: 'border',
+		title: 'Details',
+		border: false,
+        activeTab: 0,
+        plain:false,
+		items: [ { title: '<b>Details</b>', html: '', id: 'aero_details_tab'}
+				//, this.aeroRightCol
+		]
+});
+
+this.tabPanel = new Ext.TabPanel({
+        //layout: 'accordion',
+		border: false,
+        activeTab: 0,
+        plain:false,
+		height: 600,
+        defaults:{autoScroll: true},
+        items:[
+				//this.grid,
+				this.aeroPanel,
+				
+				{ title: '<b>Splash</b>', html: '<img src="images/no_image.gif" id="aero_splash">'},
+				{ title: '<b>Help</b>', html: '<div id="aero_help_tab">'},
+        ]
+	
+});   
+
+
+
+this.aeroMainWidget = new Ext.Panel({
+	//layout: 'border',
+	border: false,
+	title: '--',
+	renderTo: 'aero_main_widget',
+	plain: false,
+	items: [ this.tabPanel ]
+});
+
+
 
 
 
