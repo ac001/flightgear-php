bb07a15e631db7fd606a39a25ff7953c8b384f6a
Peter Mash Morgan <ac001@daffodil.uk.com>
2010-04-05 08:12:07 +0100

    css style tidy

--- slide_shows/index.php
+++ slide_shows/index.php
@@ -2,13 +2,16 @@
 
 require_once('../config/config.inc.php');
 
+//** Keeping self contained
+$template_path = SITE_ROOT.'slide_shows/';
+
 $slide_shows = array();
-$slide_shows['index'] = array('title' => 'FlightGear Slide Shows', 'author' => 'Pete Morgan');
-$slide_shows['curcuit'] =  array('title' => 'Tutorial: Flying A Circuit', 'author' => 'David Messingon');
+$slide_shows['index'] = array('title' => 'FlightGear Slide Shows', 'author' => '');
+$slide_shows['circuit'] =  array('title' => 'Tutorial: Flying A Circuit', 'author' => 'David Messingon');
 
 
-$slide_shows['tutorial'] = array('title' => 'Tutorial: Cross Country', 'author' => 'SSSS');
-$slide_shows['ksfo_klax'] = array('title' => 'Tutorial: KSFO to KLAX', 'author' => 'SSSS');
+//$slide_shows['tutorial'] = array('title' => 'Tutorial: Cross Country', 'author' => 'SSSS');
+//$slide_shows['ksfo_klax'] = array('title' => 'Tutorial: KSFO to KLAX', 'author' => 'SSSS');
 $slide_shows['howto_slideshow'] = array('title' => 'How to create a Slide Show', 'author' => 'Pete Morgan');
 
 
@@ -19,12 +22,12 @@ $var = isset($_GET['slide_show']) && $_GET['slide_show'] != '' ? $_GET['slide_sh
 $slide_show = $var && array_key_exists($var, $slide_shows) ? $var : 'index';
 
 //* absolute path as not in templates/
-$template_file = SITE_ROOT.'slide_shows/'.$slide_show.'/index.html'; 
+$template_file = $template_path.$slide_show.'/index.html'; 
 $show = $slide_shows[$slide_show];
 
 $smarty->assign('site_title', 'FlightGear Slide Shows');
 $smarty->assign('template_file', $template_file);
 $smarty->assign('show', $show);
 $smarty->assign('slide_shows', $slide_shows);
-$smarty->display('slide_show_container.html');
+$smarty->display($template_path.'slide_show_container.html');
 ?>
